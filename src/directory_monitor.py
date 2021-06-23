f63ba581eda6536e73ffd62fd4320950627fcd13
Cole Stanfield <dev@jacksonmed.org>
2021-06-23 16:35:19 -0700

    added message methods

--- src/directory_monitor.py
+++ src/directory_monitor.py
@@ -64,14 +64,10 @@ if __name__ == '__main__':
         path = "/home/pi/Desktop/sensor_data"
     else:
         path = "/home/dev/Desktop/sensor_data"
-    if len(sys.argv) == 1:
-        p = Patient()
-        bed = Bed(patient=p)
-        watch = OnMyWatch(bed=bed, path=path)
-        watch.run()
-    elif sys.argv[1] == 'message':
-        print("Entering Message Class")
-        message = Message()
-        message.message()
-    else:
-        print("Invalid argument passed")
+    p = Patient()
+    bed = Bed(patient=p)
+    watch = OnMyWatch(bed=bed, path=path)
+    watch.run()
+    # print("Entering Message Class")
+    # message = Message()
+    # message.message()
