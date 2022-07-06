6ab6ecbc5936009f547d66fbc3836793864a7113
xuefugod <xuefu0618@gmail.com>
2022-07-05 18:26:22 -0700

    feature/US113_Integrate_Body_Identification_Model (intergration): updated for hashmap unit testing

--- tests/test_model.py
+++ tests/test_model.py
@@ -19,8 +19,12 @@ sys.path.append("../src/decision_algorithm/ml")
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(preprocessing.convert_to_image(TEST_DIR), True)  # add assertion here
-        self.assertEqual(model.Model().load_model(IMAGE_DIR,MODEL_DIR), {'head': [[10, 51], [10, 56], [15, 51], [15, 56]], 'shoulder': [], 'buttocks': [], 'leg': [[42, 42], [42, 49], [55, 42], [55, 49], [42, 53], [42, 59], [54, 53], [54, 59]], 'arm': [[8, 61], [8, 65], [14, 61], [14, 65], [9, 38], [9, 42], [13, 38], [13, 42]], 'heel': []})  # add assertion here
+        a = {"head": [[[10, 51], [10, 56], [15, 51], [15, 56]]]}
+        #print(a.items())
+        #print(model.Model().load_model(IMAGE_DIR,MODEL_DIR).items())
 
+        self.assertEqual(model.Model().load_model(IMAGE_DIR,MODEL_DIR), {'head': [[[10, 51], [10, 56], [15, 51], [15, 56]]], 'shoulder': [], 'buttocks': [], 'leg': [[[42, 42], [42, 49], [55, 42], [55, 49]], [[42, 53], [42, 59], [54, 53], [54, 59]]], 'arm': [[[8, 61], [8, 65], [14, 61], [14, 65]], [[9, 38], [9, 42], [13, 38], [13, 42]]], 'heel': []})  # add assertion here
+        self.assertLessEqual(a.items(), model.Model().load_model(IMAGE_DIR,MODEL_DIR).items())
 if __name__ == '__main__':
     unittest.main()
     #model.load_model()
